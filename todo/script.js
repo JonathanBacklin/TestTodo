2477f574993c4171a7a2f5aaf462c08ffb037dbd
JonathanBacklin <Jonbac020809@student.jenseneducation.se>
2021-11-11 13:15:39 +0100

    Add files via upload

--- todo/script.js
+++ todo/script.js
@@ -1,4 +1,3 @@
-const addButton = document.querySelector('addButton');
 let inputValue = document.querySelector('.input');
 const container = document.querySelector('.container');
 const secondContainer = document.querySelector('.second-container');
@@ -8,21 +7,20 @@ class item{
     constructor(itemName){
         this.createDiv(itemName);
     }
-
     createDiv(itemName){
         let input = document.createElement('input');
         input.value = itemName;
         input.disabled = true;
         input.classList.add('item_input');
         input.type = "text";
-
+        
         let itemBox = document.createElement('div');
         itemBox.classList.add('item')
-
 
         let editButton = document.createElement('button')
         editButton.classList.add('editButton')
         editButton.innerHTML = "EDIT"
+
         let completedButton = document.createElement('button')
         completedButton.classList.add('completeButton')
         completedButton.innerHTML = "COMPLETE"
@@ -32,7 +30,6 @@ class item{
         removeButton.innerHTML = "REMOVE"
 
         container.appendChild(itemBox);
-
         itemBox.appendChild(input);
         itemBox.appendChild(editButton);
         itemBox.appendChild(completedButton);
@@ -41,50 +38,52 @@ class item{
         editButton.addEventListener('click', () => this.edit(input))
         completedButton.addEventListener('click', () => this.move(itemBox))
         removeButton.addEventListener('click', () => this.remove(itemBox))
-        console.log(itemBox.parentNode)
+        removeButton.addEventListener('click', () => this.test(itemBox))
     }
     edit(input){
-        input.disabled = !input.disabled
-        input.classList.toggle('item_clicked')
+        if(input.value == ""){
+            error.innerHTML = "Can't leave it empty!"
+            error.hidden = false
+        }
+        else{
+            input.classList.toggle('item_clicked')
+            input.disabled = !input.disabled
+            error.hidden = true
+        }
         
     }
     move(itemBox){
         secondContainer.append(itemBox)
-        
+        let test = itemBox.childNodes[2]
+        itemBox.removeChild(test)
+
     }
     remove(item){
         container.removeChild(item)
     }
+    test(item){
+        secondContainer.removeChild(item)
+    }
 }
 
-
-let addFunction = (inputValue) => {
     
-
+let addFunction = (inputValue) => {
+    inputValue = document.querySelector('.input').value;
     if (inputValue == ""){
-        error.innerHTML = "Error"
+        error.innerHTML = "You need to type something!"
         error.hidden = false
     }
     else {
         new item(inputValue)
+        error.hidden = true
     }
-
-    inputValue = "";
+    inputValue = document.querySelector('.input').value = ""
 }
 
 let resetFunction = () => {
     container.innerHTML = ""
     secondContainer.innerHTML = ""
+    error.hidden = true
 }
-// function check(){
-//     if(inputValue.value != ""){
-//         new item(inputValue.value);
-//         inputValue.value = "";
-//     }
-//  }
-
-// addButton.addEventListener('click', () => {
-
-// });
 
 
